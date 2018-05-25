5033f0baf774895350b656d3724c99cc2bd49717
Tyson Battistella <tysonbattistella@gmail.com>
2018-05-24 20:16:44 -0700

    conditionally show tags/authors if they exist

--- src/components/Publication.js
+++ src/components/Publication.js
@@ -18,19 +18,33 @@ export default class Publication extends React.PureComponent {
   render(){
     const {publication} = this.props;
     const {title, pdf, arxiv_url, abstract, authors, subjects} = publication;
+
+    const Subjects = () => {
+      if(subjects.filter(s=>s.name!==null).length){
+        return (<Text color={'darkGrey'}><i className={'fa fa-tags'}/> {subjects.filter(s=>s.name!==null).map(s => s.name).join(', ')}</Text>);
+      }
+      return null;
+    };
+    let Authors = () => {
+      if (authors.length){
+        return (<Text color={'darkGrey'}>{authors.map(a => a.name).join(', ')}</Text>)
+      }
+      return null;
+    };
+
     return (
       <Flex py={2} px={1} flexWrap={'wrap'}>
         <Box color={'text'} p={1} borderRadius={4} w={1}>
           <LatexText fontSize={[2,3]} parentEl={Heading}>{title}</LatexText>
         </Box>
         <Box ml='auto' py={1}>
-          <Text color={'darkGrey'}>{authors.map(a => a.name).join(', ')}</Text>
+          <Authors/>
         </Box>
         <Box w={1}>
           <LatexText fontSize={[1, 2]} parentEl={Text}>{abstract}</LatexText>
         </Box>
         <Box w={1} py={2}>
-          <Text color={'darkGrey'}><i className={'fa fa-tags'}/> {subjects.map(s => s.name).join(', ')}</Text>
+          <Subjects/>
         </Box>
         <Box w={1}>
           <Link href={arxiv_url} target={'_blank'}><i className="fa fa-external-link-alt"/> View on ArXiv</Link>&nbsp;-&nbsp;
